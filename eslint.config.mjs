96c34f8c1b8eedd84ebf7e46071530d21d58c204
lightspot-coder <214618438+lightspot-coder@users.noreply.github.com>
2025-11-18 10:28:41 +0100

    adding prettier to the template repository

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,11 +3,23 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   {
-		rules: {
-			"no-unused-vars": "warn",
-			"no-undef": "warn",
-		},
-	},
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
+  // matches all files ending with .js
+  {
+    files: ["**/*.js"],
+    rules: {
+      semi: "error",
+    },
+  },
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn",
+    },
+  },
 ]);
